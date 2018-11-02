db71a671e384e1593a3cc4fe823c54308022d251
Maciej4 <m@maciej.me>
2018-11-02 15:46:40 -0700

    arcade drive

--- src/main/java/org/usfirst/frc/team972/Robot.java
+++ src/main/java/org/usfirst/frc/team972/Robot.java
@@ -4,30 +4,58 @@ import edu.wpi.first.wpilibj.*;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class Robot extends IterativeRobot {
 	
+	//Talon Declarations
 	WPI_TalonSRX right1;
 	WPI_TalonSRX left1;
 	WPI_TalonSRX right2;
 	WPI_TalonSRX left2;
-	Joystick joy;
-	
-	double rightPower;
-	double leftPower;
-	
 	//WPI_TalonSRX rollyGrabberMotorOne;
 	//WPI_TalonSRX rollyGrabberMotorTwo;
 	
+	//Groups for each side
+	SpeedControllerGroup leftGroup;
+	SpeedControllerGroup rightGroup;
+	
+	//Differential Drive for main control
+	DifferentialDrive mainDrive;
+	
+	//Sample encoder declaration
+	//Encoder encoderExample;
+	
+	//Joystick Declaration
+	Joystick joy;
+	
+	//Doubles for drive control
+	double power;
+	double turning;
+	
 	//Runs once on robot start
 	public void robotInit() {
+		//Initialization of left side
 		left1 = new WPI_TalonSRX(1);
 		left2 = new WPI_TalonSRX(2);
+		leftGroup = new SpeedControllerGroup(left1, left2);
+		
+		//Initialization of right side
 		right1 = new WPI_TalonSRX(4);
 		right2 = new WPI_TalonSRX(5);
+		rightGroup = new SpeedControllerGroup(right1, right2);
+		
+		//Initialization of main drive
+		mainDrive = new DifferentialDrive(leftGroup, rightGroup);
+		
+		//Sample encoder initialization
+		//encoderExample = new Encoder(1,2);
+		
+		//Intake motor initializations
 		//rollyGrabberMotorOne = new WPI_TalonSRX(7);
 		//rollyGrabberMotorTwo = new WPI_TalonSRX(8);
 		
+		//Joystick initialization
 		joy = new Joystick(0);
 	}
 	
@@ -36,7 +64,7 @@ public class Robot extends IterativeRobot {
 		
 	}
 	
-	//Runs repeatedly on auto
+	//Runs repeatedly on auto period
 	public void autonomousPeriodic() {
 		
 	}
@@ -46,14 +74,16 @@ public class Robot extends IterativeRobot {
 		
 	}
 	
-	//Runs repeatedly on teleop
+	//Runs repeatedly on teleop period
 	public void teleopPeriodic() {
-		leftPower = joy.getRawAxis(1);
-		rightPower = -joy.getRawAxis(5);
+		//Read joystick values
+		power = joy.getY();
+		turning = joy.getX();
 		
-		right1.set(rightPower);
-		right2.set(rightPower);
-		left1.set(leftPower);
-		left2.set(leftPower);
+		//Run the differential drive in arcade mode
+		mainDrive.arcadeDrive(power, turning);
 	}
 }
+
+
+
