87b15ee89aa123b30b3463e89b29751ae0dbdc0c
Maciej4 <m@maciej.me>
2018-11-02 15:30:15 -0700

    basic tank drive

--- src/main/java/org/usfirst/frc/team972/Robot.java
+++ src/main/java/org/usfirst/frc/team972/Robot.java
@@ -7,21 +7,28 @@ import edu.wpi.first.wpilibj.Joystick;
 
 public class Robot extends IterativeRobot {
 	
-	WPI_TalonSRX right;
-	WPI_TalonSRX left;
-	Joystick myJoystick;
+	WPI_TalonSRX right1;
+	WPI_TalonSRX left1;
+	WPI_TalonSRX right2;
+	WPI_TalonSRX left2;
+	Joystick joy;
 	
-	WPI_TalonSRX rollyGrabberMotorOne;
-	WPI_TalonSRX rollyGrabberMotorTwo;
+	double rightPower;
+	double leftPower;
+	
+	//WPI_TalonSRX rollyGrabberMotorOne;
+	//WPI_TalonSRX rollyGrabberMotorTwo;
 	
 	//Runs once on robot start
 	public void robotInit() {
-		left = new WPI_TalonSRX(1);
-		right = new WPI_TalonSRX(4);
-		rollyGrabberMotorOne = new WPI_TalonSRX(7);
-		rollyGrabberMotorTwo = new WPI_TalonSRX(8);
+		left1 = new WPI_TalonSRX(1);
+		left2 = new WPI_TalonSRX(2);
+		right1 = new WPI_TalonSRX(4);
+		right2 = new WPI_TalonSRX(5);
+		//rollyGrabberMotorOne = new WPI_TalonSRX(7);
+		//rollyGrabberMotorTwo = new WPI_TalonSRX(8);
 		
-		myJoystick = new Joystick(0);
+		joy = new Joystick(0);
 	}
 	
 	//Runs once on auto start
@@ -41,17 +48,12 @@ public class Robot extends IterativeRobot {
 	
 	//Runs repeatedly on teleop
 	public void teleopPeriodic() {
-		double rightPower = myJoystick.getRawAxis(5);
-		double leftPower = myJoystick.getRawAxis(1);
+		leftPower = joy.getRawAxis(1);
+		rightPower = -joy.getRawAxis(5);
 		
-		System.out.println("Left Power: " + leftPower);
-		System.out.println("Right Power: " + rightPower);
-		right.set(ControlMode.PercentOutput, rightPower);
-		left.set(ControlMode.PercentOutput, -leftPower);
-		
-		double rollyGrabberMotorSpeed = 0.5;
-		
-		rollyGrabberMotorOne.set(rollyGrabberMotorSpeed);
-		rollyGrabberMotorTwo.set(rollyGrabberMotorSpeed);
+		right1.set(rightPower);
+		right2.set(rightPower);
+		left1.set(leftPower);
+		left2.set(leftPower);
 	}
 }
