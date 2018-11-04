8d9a75c9f02dda458fb970c875916ca60b7f428c
Holden Adamec <Holden.Adamec@gmail.com>
2018-11-03 17:50:05 -0700

    Finished implementing the intake task for teams a and c

--- src/main/java/org/usfirst/frc/team972/executor/teleop/TeleopIntakeTask.java
+++ src/main/java/org/usfirst/frc/team972/executor/teleop/TeleopIntakeTask.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team972.executor.teleop;
 import org.usfirst.frc.team972.executor.Task;
 import org.usfirst.frc.team972.motors.MechanismActuators;
 import org.usfirst.frc.team972.ui.UserInputGamepad;
+import org.usfirst.frc.team972.util.Constants;
 
 public class TeleopIntakeTask extends Task {
 	UserInputGamepad uig;
@@ -17,19 +18,39 @@ public class TeleopIntakeTask extends Task {
 	@Override
 	public void init(double dt) {
 //		Only use if on team C
-//		mechanismActuators.setupIntakeMotors(intakeMotorID);
+//		mechanismActuators.setupIntakeMotors(Constants.INTAKE_MOTOR_ID);
 		
 //		Only use if on team A
-//		mechanismActuators.setupIntakeMotors(intakeMotorID_1, intakeMotorID_2);
+//		mechanismActuators.setupIntakeMotors(Constants.INTAKE_MOTOR_ID_1, Constants.INTAKE_MOTOR_ID_2);
 	}
 
 	@Override
 	public void execute(double dt) {
+		double speed = 0.5; // Change this to whatever works
+		
 //		Only use if on team C
-//		mechanismActuators.runSingleMotorIntake(speed);
+		/*
+		if (uig.getDriverStick().getRawButtonPressed(Constants.INTAKE_BUTTON_ID)) {
+			mechanismActuators.runSingleMotorIntake(speed);
+		} else if (uig.getDriverStick().getRawButtonPressed(Constants.OUTTAKE_BUTTON_ID)) {
+//			runs intake backwards
+			mechanismActuators.runSingleMotorIntake(-speed);
+		} else {
+			mechanismActuators.runSingleMotorIntake(0);
+		}
+		*/
 		
 //		Only use if on team A
-//		mechanismActuators.runDoubleMotorIntake(speed);
+		/*
+		if (uig.getDriverStick().getRawButtonPressed(Constants.INTAKE_BUTTON_ID)) {
+			mechanismActuators.runDoubleMotorIntake(speed);
+		} else if (uig.getDriverStick().getRawButtonPressed(Constants.OUTTAKE_BUTTON_ID)) {
+//			runs intake backwards
+			mechanismActuators.runDoubleMotorIntake(-speed);
+		} else {
+			mechanismActuators.runDoubleMotorIntake(0);
+		}
+		*/
 	}
 
 }
--- src/main/java/org/usfirst/frc/team972/util/Constants.java
+++ src/main/java/org/usfirst/frc/team972/util/Constants.java
@@ -5,6 +5,13 @@ public class Constants {
 	public static final double DISTANCE_PER_PULSE = 1;
 	
 	public static final int INTAKE_BUTTON_ID = 1; // Replace with desired button id!
-	public static final int OUTTAKE_BUTTON_ID = 1; // Replace with desired button id! - Team A only!
+	
+//	For Team A
+	public static final int OUTTAKE_BUTTON_ID = 1; // Replace with desired button id!
+	public static final int INTAKE_MOTOR_ID_1 = 7;
+	public static final int INTAKE_MOTOR_ID_2 = 8;
+	
+//	For Team C
+	public static final int INTAKE_MOTOR_ID = 0; // Replace with desired motor id!
 	
 }
