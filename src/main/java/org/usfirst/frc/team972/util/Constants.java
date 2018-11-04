48b348dd38b08bb96031a440475997af7015baca
Holden Adamec <Holden.Adamec@gmail.com>
2018-11-03 17:52:15 -0700

    Made the unset constants -1

--- src/main/java/org/usfirst/frc/team972/util/Constants.java
+++ src/main/java/org/usfirst/frc/team972/util/Constants.java
@@ -4,14 +4,14 @@ public class Constants {
 
 	public static final double DISTANCE_PER_PULSE = 1;
 	
-	public static final int INTAKE_BUTTON_ID = 1; // Replace with desired button id!
+	public static final int INTAKE_BUTTON_ID = -1; // Replace with desired button id!
 	
 //	For Team A
-	public static final int OUTTAKE_BUTTON_ID = 1; // Replace with desired button id!
+	public static final int OUTTAKE_BUTTON_ID = -1; // Replace with desired button id!
 	public static final int INTAKE_MOTOR_ID_1 = 7;
 	public static final int INTAKE_MOTOR_ID_2 = 8;
 	
 //	For Team C
-	public static final int INTAKE_MOTOR_ID = 0; // Replace with desired motor id!
+	public static final int INTAKE_MOTOR_ID = -1; // Replace with desired motor id!
 	
 }
